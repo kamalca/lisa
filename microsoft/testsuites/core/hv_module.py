4f4edcd40d306ed1f072be06d86d296e3dcab1aa
Kameron Carr <kameroncarr@microsoft.com>
2021-09-07 17:09:32 -0700

    Checks all hv_modules for presence
    TODO: special version cases

--- microsoft/testsuites/core/hv_module.py
+++ microsoft/testsuites/core/hv_module.py
@@ -1,33 +1,32 @@
-from lisa.operating_system import OperatingSystem
-from lisa import (
-    LisaException,
-    Logger,
-    PassedException,
-    RemoteNode,
-    SkippedException,
-    TestCaseMetadata,
-    TestSuite,
-    TestSuiteMetadata
-)
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+from assertpy import assert_that
+
+from lisa import Logger, RemoteNode, TestCaseMetadata, TestSuite, TestSuiteMetadata
+
 
 @TestSuiteMetadata(
     area="core",
     category="functional",
     description="""
-    This test suite covers test cases previously handled by LISAv2: 
-    LIS-MODULES-CHECK, VERIFY-LIS-MODULES-VERSION, INITRD-MODULES-CHECK, RELOAD-MODULES-SMP
+    This test suite covers test cases previously handled by LISAv2:
+    LIS-MODULES-CHECK, VERIFY-LIS-MODULES-VERSION,
+    INITRD-MODULES-CHECK, RELOAD-MODULES-SMP
     """,
 )
 class HvModule(TestSuite):
     @TestCaseMetadata(
         description="""
         This test case will
-        1. Verify the list of given LIS kernel modules and verify if the version 
+        1. Verify the list of given LIS kernel modules and verify if the version
         matches with the Linux kernel release number.
         """,
         priority=0,
     )
-    def verify_lis_modules_version(self, case_name: str, log: Logger, node: RemoteNode) -> None:
+    def verify_lis_modules_version(
+        self, case_name: str, log: Logger, node: RemoteNode
+    ) -> None:
         pass
 
     @TestCaseMetadata(
@@ -40,7 +39,7 @@ class HvModule(TestSuite):
     def lis_modules_check(self, case_name: str, log: Logger, node: RemoteNode) -> None:
         uname = node.execute("uname -r").stdout
         configPath = f"/boot/config-{uname}"
-        hvModules=["hv_storvsc", "hv_netvsc", "hv_vmbus", "hv_utils", "hid_hyperv"]
+        hvModules = ["hv_storvsc", "hv_netvsc", "hv_vmbus", "hv_utils", "hid_hyperv"]
         if node.execute(f"grep CONFIG_HYPERV_STORAGE=y {configPath}").exit_code == 0:
             hvModules.remove("hv_storvsc")
         if node.execute(f"grep CONFIG_HYPERV_NET=y {configPath}").exit_code == 0:
@@ -52,4 +51,16 @@ class HvModule(TestSuite):
         if node.execute(f"grep CONFIG_HID_HYPERV_MOUSE=y {configPath}").exit_code == 0:
             hvModules.remove("hid_hyperv")
 
-        print(hvModules)
+        presentModules = 0
+        output = node.execute("lsmod").stdout
+        for module in hvModules:
+            if module in output:
+                log.info(f"Module {module} present")
+                presentModules = presentModules + 1
+            else:
+                log.error(f"Module {module} absent")
+
+        print(f"{len(hvModules)} == {presentModules}")
+        assert_that(presentModules, "Not all LIS modules are present.").is_equal_to(
+            len(hvModules)
+        )
