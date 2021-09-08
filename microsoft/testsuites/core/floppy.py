fce854e6ee61068ccc58b47ae7158e86e6f8a53e
Kameron Carr <kameroncarr@microsoft.com>
2021-09-08 10:43:40 -0700

    Lsmod version

--- microsoft/testsuites/core/floppy.py
+++ microsoft/testsuites/core/floppy.py
@@ -2,7 +2,6 @@ from assertpy import assert_that
 
 from lisa import Logger, RemoteNode, TestCaseMetadata, TestSuite, TestSuiteMetadata
 from lisa.tools import Lsmod
-from lisa.util import LisaException
 
 
 @TestSuiteMetadata(
@@ -23,13 +22,7 @@ class Floppy(TestSuite):
     def check_floppy_module(
         self, case_name: str, log: Logger, node: RemoteNode
     ) -> None:
-        # cmd_result = node.execute("/usr/sbin/lsmod | /usr/bin/grep floppy", shell=True)
-        # assert_that(cmd_result.exit_code).described_as(
-        #     "Floppy module should be disabled."
-        # ).is_equal_to(1)
-
-        node.execute("sudo modprobe floppy")
-
         lsmod = node.tools[Lsmod]
-        if lsmod.module_exists(mod_name="floppy"):
-            raise LisaException("Module present")
+        assert_that(lsmod.module_exists("floppy")).described_as(
+            "Floppy module should be disabled"
+        ).is_false()
