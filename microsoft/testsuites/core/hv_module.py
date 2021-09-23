c4736b7b4b1d5a665dca74342c258539f13d1e85
Kameron Carr <kameroncarr@microsoft.com>
2021-09-23 09:56:55 -0700

    Fixes based on PR feedback

--- microsoft/testsuites/core/hv_module.py
+++ microsoft/testsuites/core/hv_module.py
@@ -2,10 +2,12 @@
 # Licensed under the MIT license.
 
 from assertpy import assert_that
+from semver import VersionInfo
 
 from lisa import Logger, RemoteNode, TestCaseMetadata, TestSuite, TestSuiteMetadata
-from lisa.operating_system import CentOs, Redhat
-from lisa.tools import Dmesg, Lsmod, Modinfo, Modprobe, Rpm, Uname
+from lisa.operating_system import Redhat
+from lisa.sut_orchestrator.azure.tools import LisDriver
+from lisa.tools import Lsmod, Modinfo, Modprobe, Uname
 from lisa.util import SkippedException
 
 
@@ -16,43 +18,12 @@ from lisa.util import SkippedException
     This test suite covers test cases previously handled by LISAv2:
     LIS-MODULES-CHECK, VERIFY-LIS-MODULES-VERSION,
     INITRD-MODULES-CHECK, RELOAD-MODULES-SMP
+
+    It is responsible for ensuring the Hyper V drivers are all present,
+    included in initrd, and all the same version.
     """,
 )
 class HvModule(TestSuite):
-    def get_modules(self, node: RemoteNode) -> list[str]:
-        """
-        Returns the hv_modules that are not directly loaded into the kernel.
-        """
-        hv_modules = [
-            "hv_storvsc",
-            "hv_netvsc",
-            "hv_vmbus",
-            "hv_utils",
-            "hid_hyperv",
-            "hv_balloon",
-            "hyperv_keyboard",
-        ]
-        uname = node.tools[Uname]
-        kernel_version = uname.get_linux_information().kernel_version_raw
-        config_path = f"/boot/config-{kernel_version}"
-
-        if node.execute(f"grep CONFIG_HYPERV_STORAGE=y {config_path}").exit_code == 0:
-            hv_modules.remove("hv_storvsc")
-        if node.execute(f"grep CONFIG_HYPERV_NET=y {config_path}").exit_code == 0:
-            hv_modules.remove("hv_netvsc")
-        if node.execute(f"grep CONFIG_HYPERV=y {config_path}").exit_code == 0:
-            hv_modules.remove("hv_vmbus")
-        if node.execute(f"grep CONFIG_HYPERV_UTILS=y {config_path}").exit_code == 0:
-            hv_modules.remove("hv_utils")
-        if node.execute(f"grep CONFIG_HID_HYPERV_MOUSE=y {config_path}").exit_code == 0:
-            hv_modules.remove("hid_hyperv")
-        if node.execute(f"grep CONFIG_HYPERV_BALLOON=y {config_path}").exit_code == 0:
-            hv_modules.remove("hv_balloon")
-        if node.execute(f"grep CONFIG_HYPERV_KEYBOARD=y {config_path}").exit_code == 0:
-            hv_modules.remove("hyperv_keyboard")
-
-        return hv_modules
-
     @TestCaseMetadata(
         description="""
         This test case will
@@ -64,24 +35,25 @@ class HvModule(TestSuite):
     def verify_lis_modules_version(
         self, case_name: str, log: Logger, node: RemoteNode
     ) -> None:
-        if not (isinstance(node.os, CentOs) or isinstance(node.os, Redhat)):
-            log.info("Distro not supported")
+        if not isinstance(node.os, Redhat):
+            log.info(f"{node.os.name} not supported")
             raise SkippedException
 
-        hv_modules = self.get_modules(node)
-        rpm = node.tools[Rpm]
-        lis_installed = rpm.is_package_installed("microsoft-hyper-v")
-
         modinfo = node.tools[Modinfo]
-        dmesg = node.tools[Dmesg]
-        lis_version = dmesg.get_lis_version()
+        lis_driver = node.tools[LisDriver]
+        lis_version = lis_driver.get_version()
+        hv_modules = self.__get_modules_not_in_kernel(node)
+        lis_installed = node.os.package_exists("microsoft-hyper-v")
 
         if lis_installed:
             for module in hv_modules:
-                module_version = modinfo.get_version(module)
+                module_version = VersionInfo.parse(modinfo.get_version(module))
                 assert_that(lis_version == module_version).described_as(
                     f"Version of {module} is {module_version}, expected {lis_version}"
                 ).is_true()
+                assert_that(module_version).described_as(
+                    f"Version of {module} does not match LIS version"
+                ).is_equal_to(lis_version)
 
     @TestCaseMetadata(
         description="""
@@ -90,41 +62,39 @@ class HvModule(TestSuite):
         """,
         priority=0,
     )
-    def lis_modules_check(self, case_name: str, log: Logger, node: RemoteNode) -> None:
-        hv_modules = self.get_modules(node)
+    def verify_hyperv_modules(
+        self, case_name: str, log: Logger, node: RemoteNode
+    ) -> None:
+        hv_modules = self.__get_modules_not_in_kernel(node)
         distro_version = node.os.information.version
 
-        if isinstance(node.os, CentOs) or isinstance(node.os, Redhat):
-            rpm = node.tools[Rpm]
+        if isinstance(node.os, Redhat):
             modprobe = node.tools[Modprobe]
-            lis_installed = rpm.is_package_installed("microsoft-hyper-v")
+            lis_installed = node.os.package_exists("microsoft-hyper-v")
 
-            if distro_version >= "4.3.0" and lis_installed:
+            if lis_installed:
                 hv_modules.append("pci_hyperv")
                 modprobe.run("pci_hyperv", sudo=True)
 
             if (
                 distro_version >= "7.3.0" or distro_version < "7.5.0"
             ) and lis_installed:
-                dmesg = node.tools[Dmesg]
-                lis_version = dmesg.get_lis_version()
-                if lis_version >= "4.3.0":
-                    hv_modules.append("mlx4_en")
-                    modprobe.run("mlx4_en", sudo=True)
+                hv_modules.append("mlx4_en")
+                modprobe.run("mlx4_en", sudo=True)
 
-        present_modules = 0
+        num_modules_loaded = 0
         lsmod = node.tools[Lsmod]
         for module in hv_modules:
             if lsmod.module_exists(module):
                 log.info(f"Module {module} present")
-                present_modules = present_modules + 1
+                num_modules_loaded = num_modules_loaded + 1
             else:
                 log.error(f"Module {module} absent")
 
-        print(f"{len(hv_modules)} == {present_modules}")
-        assert_that(present_modules, "Not all LIS modules are present.").is_equal_to(
-            len(hv_modules)
-        )
+        num_modules_expected = len(hv_modules)
+        assert_that(num_modules_loaded).described_as(
+            "Not all Hyper V drivers are present."
+        ).is_equal_to(num_modules_expected)
 
     @TestCaseMetadata(
         description="""
@@ -137,7 +107,7 @@ class HvModule(TestSuite):
     def initrd_modules_check(
         self, case_name: str, log: Logger, node: RemoteNode
     ) -> None:
-        hv_modules = self.get_modules(node)
+        hv_modules = self.__get_modules_not_in_kernel(node)
         uname = node.tools[Uname]
         kernel_version = uname.get_linux_information().kernel_version_raw
         # TODO: Install dracut
@@ -155,3 +125,31 @@ class HvModule(TestSuite):
         )
         for module in hv_modules:
             print(module)
+
+    def __get_modules_not_in_kernel(self, node: RemoteNode) -> list[str]:
+        """
+        Returns the hv_modules that are not directly loaded into the kernel.
+        """
+        hv_modules_configuration = {
+            "hv_storvsc": "CONFIG_HYPERV_STORAGE",
+            "hv_netvsc": "CONFIG_HYPERV_NET",
+            "hv_vmbus": "CONFIG_HYPERV",
+            "hv_utils": "CONFIG_HYPERV_UTILS",
+            "hid_hyperv": "CONFIG_HID_HYPERV_MOUSE",
+            "hv_balloon": "CONFIG_HYPERV_BALLOON",
+            "hyperv_keyboard": "CONFIG_HYPERV_KEYBOARD",
+        }
+        uname = node.tools[Uname]
+        kernel_version = uname.get_linux_information().kernel_version_raw
+        config_path = f"/boot/config-{kernel_version}"
+
+        for module in hv_modules_configuration:
+            if (
+                node.execute(
+                    f"grep {hv_modules_configuration[module]}=y {config_path}"
+                ).exit_code
+                == 0
+            ):
+                hv_modules_configuration.pop(module)
+
+        return [*hv_modules_configuration]
